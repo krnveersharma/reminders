0512b4a45e4066732cfab28fcf11cc0ca4558ee6
krnveersharma <karansharma10293847@gmail.com>
2025-03-14 12:15:23 +0530

    login

--- controllers/userControllers/auth.go
+++ controllers/userControllers/auth.go
@@ -5,15 +5,50 @@ import (
 	"fmt"
 
 	"github.com/reminders/internal/domain"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 func Register(user domain.User, db *gorm.DB) error {
-	registeredUser := db.Create(&user)
-	if registeredUser.Error != nil {
-		fmt.Errorf("Error in creating user: %v", registeredUser.Error)
-		return errors.New("error in registering user")
+	existingUser, err := getUser(user.Email, db)
+
+	if existingUser.Email != "" {
+		fmt.Printf("Register:user already exist\n")
+		return errors.New("user already exists")
+	}
+	if err != nil {
+		return err
 	}
 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		fmt.Printf("Error hashing password: %s\n", err.Error())
+		return errors.New("unable to generate hash of password")
+	}
+	user.Password = string(hashedPassword)
+
+	insertQuery := "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)"
+	result := db.Exec(insertQuery, user.FirstName, user.LastName, user.Email, user.Password)
+
+	if result.Error != nil {
+		fmt.Printf("Error inserting user: %v\n", result.Error)
+		return errors.New("unable to register user")
+	}
+
+	fmt.Println("User registered successfully:", user.Email)
 	return nil
+}
+
+func getUser(email string, db *gorm.DB) (domain.User, error) {
+	var existingUser domain.User
+
+	query := "SELECT * FROM users WHERE email = ?"
+	result := db.Raw(query, email).Scan(&existingUser)
+
+	if result.Error != nil {
+		fmt.Errorf("Error in executing sql query:", result.Error)
+		return domain.User{}, errors.New("Some error occured")
+	}
+
+	return existingUser, nil
 }
--- controllers/userControllers/userRoutes.go
+++ controllers/userControllers/userRoutes.go
@@ -1,21 +1,31 @@
 package usercontrollers
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/reminders/internal/domain"
+	"github.com/reminders/internal/dto"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type userRoutes struct {
-	db *gorm.DB
+	db     *gorm.DB
+	Secret string
 }
 
-func SetUpUserRoutes(router *gin.RouterGroup, db *gorm.DB) {
-	routes := &userRoutes{db: db}
+func SetUpUserRoutes(router *gin.RouterGroup, db *gorm.DB, secret string) {
+	routes := &userRoutes{
+		db:     db,
+		Secret: secret,
+	}
 	router.POST("/register", routes.RegisterUser)
+	router.POST("/login", routes.login)
 }
 
 func (r *userRoutes) RegisterUser(ctx *gin.Context) {
@@ -27,9 +37,53 @@ func (r *userRoutes) RegisterUser(ctx *gin.Context) {
 	}
 	err := Register(user, r.db)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "User registered successfully", "user": user})
+}
+
+func (r *userRoutes) login(ctx *gin.Context) {
+	var user dto.UserLogin
+	if err := ctx.ShouldBindBodyWithJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"error": "Invalid user data"})
+		return
+	}
+
+	getUser, err := getUser(user.Email, r.db)
+	fmt.Printf("user is:", getUser)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(getUser.Password), []byte(user.Password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	jwtToken, err := createToken(getUser, r.Secret)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": jwtToken})
+}
+
+func createToken(user domain.User, secret string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"id":    user.ID,
+			"email": user.Email,
+			"exp":   time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		fmt.Errorf("Error in jwt token parsing: %v", err)
+		return "", errors.New("some error occured")
+	}
+
+	return tokenString, nil
 }
